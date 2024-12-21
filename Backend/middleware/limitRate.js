976c421a12e95b7cac38dddd2a60f5d64e2ef85d
mcaballero879 <mcaballero879@gmail.com>
2024-12-21 18:04:29 -0300

    se agrego logger a validateSchema

--- Backend/middleware/limitRate.js
+++ Backend/middleware/limitRate.js
@@ -1,11 +1,14 @@
 import rateLimit from 'express-rate-limit'
+import { Logger } from 'winston';  
 
 export const loginLimit = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutos
     max: 10, // número máximo de intentos de inicio de sesión permitidos dentro del período de tiempo
-    handler: (req, res) => {
+    handler: (req, res, next) => {
         res.status(429).json({
             message: 'Demasiados intentos de inicio de sesión. Por favor, inténtalo de nuevo más tarde.'
         });
+        logger.info(`${req.metod} ${req.url}`)
+        next()
     }
 })
--- Backend/middleware/validateSchema.js
+++ Backend/middleware/validateSchema.js
@@ -1,3 +1,4 @@
+import { logger } from "../config/Winston.js"
 export const validateSchema = (schema) => (req, res, next) => {
     try {
         schema.parse(req.body)//los datos registros son enviados para luego ser validados por zod
@@ -5,8 +6,12 @@ export const validateSchema = (schema) => (req, res, next) => {
     } catch (error) {
         if (error) {
             return res.status(400).json({ error: error.errors.map(error => error.message) }) //recopila el error de zod luego es enviado al frontend
+            logger.info(`${req.metod} ${req.url}`)
+            next() 
         } else {
             return res.status(500).json({error: 'Error interno del servidor al validar los datos'})
+            logger.info(`${req.metod} ${req.url}`)
+            next() 
         }
     }
 }
